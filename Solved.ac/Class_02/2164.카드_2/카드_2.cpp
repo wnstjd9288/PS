e685d436a8ed59d008d6a53a6393fe8fe0f2e411
wnstjd9288 <wnstjd9288@naver.com>
2025-03-11 16:42:30 +0900

    250311 카드_2

---
+++ Solved.ac/Class_02/2164.카드_2/카드_2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int que[10000001] = {
+        0,
+    };
+    int h = 0, t = -1;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        que[++t] = i;
+    }
+    while (h != t)
+    {
+        h++;
+        que[++t] = que[h++];
+    }
+    printf("%d", que[t]);
+    return 0;
+}
