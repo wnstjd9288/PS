74c7428792654ee256c2cd58937df66e42015559
wnstjd9288 <wnstjd9288@naver.com>
2025-03-12 17:54:38 +0900

    250312 279B

--- Codeforce/problems/279B. Books.cpp
+++ Codeforce/problems/279B. Books.cpp
@@ -1,47 +1,27 @@
 #include <stdio.h>
-int n, m;
-int arr[100001];
-int sum[100001] = {
-    0,
-};
-int max = -1;
-void two_pointer(int start, int end)
-{
-    if (start == end && arr[end] > m)
-        return;
-    if (sum[start] - sum[end] <= m)
-    {
-        max = max > end - start ? max : end - start;
-        return;
-    }
 
-    if (sum[start] - sum[start + 1] > sum[end - 1] - sum[end])
-    {
-        two_pointer(start + 1, end);
-    }
-    else if (sum[start] - sum[start + 1] == sum[end - 1] - sum[end])
-    {
-        two_pointer(start + 1, end);
-        two_pointer(start, end - 1);
-    }
-    else
-    {
-        two_pointer(start, end - 1);
-    }
-}
 int main()
 {
+    int n, m;
+    int arr[100001];
+    int sum = 0, cnt = 0;
+    int res = 0;
     scanf("%d %d", &n, &m);
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
-        sum[i] = arr[i];
     }
-    for (int i = n - 1; i >= 1; i--)
+    for (int i = 0; i < n; i++)
     {
-        sum[i - 1] += sum[i];
+        sum += arr[i];
+        while (1)
+        {
+            if (sum <= m)
+                break;
+            sum -= arr[cnt++];
+        }
+        res = res > i - cnt + 1 ? res : i - cnt + 1;
     }
-    two_pointer(0, n);
-    printf("%d", max);
+    printf("%d", res);
     return 0;
 }
