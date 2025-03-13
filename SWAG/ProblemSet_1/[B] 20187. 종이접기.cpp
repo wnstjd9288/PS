1f7e4ac8b5c640ac30fa9ed11557d4788d567194
wnstjd9288 <wnstjd9288@naver.com>
2025-03-13 23:31:09 +0900

    250313 SWAG ProblemSet_01 [B]

--- SWAG/ProblemSet_1/[B] 20187. 종이접기.cpp
+++ SWAG/ProblemSet_1/[B] 20187. 종이접기.cpp
@@ -5,7 +5,7 @@ typedef struct
     int row;
     int column;
 } grid;
-grid map[256][256] = {
+int map[256][256] = {
     0,
 };
 char s[16];
@@ -14,44 +14,89 @@ int main()
     int n, m;
     int now = 1;
     int scale = 1;
-    grid cnt = 0, start;
+    grid cnt = {0, 0}, start = {0, 0}, end;
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
         scale *= 2;
+    end = {scale, scale};
     for (int i = 0; i < n * 2; i++)
     {
         scanf("%*c%1c", &s[i]);
-    }
-    scanf("%d", &m);
-    for (int i = 0; i < scale; i++)
-    {
-        for (int j = 0; j < scale; j++)
-        {
-            map[i][j].column = j;
-            map[i][j].row = i;
-        }
-    }
-    for (int i = 0; i < n * 2; i++)
-    {
         switch (s[i])
         {
         case 'R':
-            for (int j = 0; j < scale; j++)
-            {
-                for (int k = 0; k < scale / 2; k++)
-                {
-                }
-            }
+            start.row = (start.row + end.row) / 2;
             break;
         case 'D':
+
+            start.column = (start.column + end.column) / 2;
             break;
         case 'L':
+            end.row = (start.row + end.row) / 2;
             break;
         case 'U':
+            end.column = (start.column + end.column) / 2;
             break;
         default:
             break;
         }
+    }
+    scanf("%d", &m);
+    map[start.column][start.row] = m;
+    for (int i = n * 2 - 1; i >= 0; i--)
+    {
+        switch (s[i])
+        {
+        case 'R':
+            for (int j = start.column; j < end.column; j++)
+            {
+                for (int k = 0; k < end.row - start.row; k++)
+                {
+                    map[j][start.row * 2 - end.row + k] = (map[j][end.row - k - 1] * (-1) + 5) % 4;
+                }
+            }
+            start.row += start.row - end.row;
+            break;
+        case 'D':
+            for (int j = 0; j < end.column - start.column; j++)
+            {
+                for (int k = start.row; k < end.row; k++)
+                {
+                    map[start.column * 2 - end.column + j][k] = (map[end.column - j - 1][k] + 2) % 4;
+                }
+            }
+            start.column += start.column - end.column;
+            break;
+        case 'L':
+            for (int j = start.column; j < end.column; j++)
+            {
+                for (int k = 0; k < end.row - start.row; k++)
+                {
+                    map[j][end.row + k] = (map[j][end.row - k - 1] * (-1) + 5) % 4;
+                }
+            }
+            end.row += end.row - start.row;
+            break;
+        case 'U':
+            for (int j = 0; j < end.column - start.column; j++)
+            {
+                for (int k = start.row; k < end.row; k++)
+                {
+                    map[end.column + j][k] = (map[end.column - j - 1][k] + 2) % 4;
+                }
+            }
+            end.column += end.column - start.column;
+            break;
+        default:
+            break;
+        }
+        // printf("%d %c\n{%d,%d},{%d %d}\n", n * 2 - i, s[i], start.column, start.row, end.column, end.row);
+    }
+    for (int i = 0; i < scale; i++)
+    {
+        for (int j = 0; j < scale; j++)
+            printf("%d ", map[i][j]);
+        puts("");
     }
     return 0;
 }
