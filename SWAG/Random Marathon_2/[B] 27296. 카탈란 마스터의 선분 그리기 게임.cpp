02399f9fc6e92f57dd2e3eadc05ea7d45b924309
wnstjd9288 <wnstjd9288@naver.com>
2025-03-20 20:52:30 +0900

    250320 SWAG Random Marathon_02 [B](미완)

---
+++ SWAG/Random Marathon_2/[B] 27296. 카탈란 마스터의 선분 그리기 게임.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int t;
+    for (int i = 0; i < t; i++)
+    {
+        long long n;
+
+        scanf("%lld", &n);
+        if (n <= 1)
+            printf("1 0\n");
+        else
+            printf("0 1\n");
+    }
+}
