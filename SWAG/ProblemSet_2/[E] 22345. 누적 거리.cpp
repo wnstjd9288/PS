94f75382eb61f7c6f579cc0c064743e1d7fdac5c
wnstjd9288 <wnstjd9288@naver.com>
2025-03-25 13:12:44 +0900

    250320 SWAG ProblemSet_02 [E]

--- SWAG/ProblemSet_2/[E] 22345. 누적 거리.cpp
+++ SWAG/ProblemSet_2/[E] 22345. 누적 거리.cpp
@@ -1,24 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool compare(pair<int, int> a, pair<int, int> b)
+typedef long long ll;
+vector<ll> loc;
+bool compare(pair<ll, ll> a, pair<ll, ll> b)
 {
     return a.second < b.second;
 }
+ll lo_bound(ll s, ll e, ll k)
+{
+    ll mid;
+    while (s < e)
+    {
+        mid = (s + e) / 2;
+        if (loc[mid] < k)
+            s = mid + 1;
+        else
+            e = mid;
+    }
+    return e;
+}
 int main()
 {
-    int n, q;
-    vector<pair<int, int>> village;
-    scanf("%d %d", &n, &q);
-    for (int i = 0; i < n; i++)
+    ll n, q;
+    ll x, a;
+    vector<pair<ll, ll>> village;
+    scanf("%lld %lld", &n, &q);
+    for (ll i = 0; i < n; i++)
     {
-        int x, a;
-        scanf("%d %d", &a, &x);
+        scanf("%lld %lld", &a, &x);
+        loc.push_back(x);
         village.push_back(make_pair(a, x));
     }
+    sort(loc.begin(), loc.end());
     sort(village.begin(), village.end(), compare);
-    for (int i = 0; i < n; i++)
+    village[0].second *= village[0].first;
+
+    for (ll i = 1; i < n; i++)
     {
-        printf("%d %d\n", village[i].first, village[i].second);
+        village[i].second = village[i - 1].second + village[i].first * village[i].second;
+        village[i].first += village[i - 1].first;
     }
+
+    // for (int i = 0; i < n; i++)
+    //     printf("%lld ", loc[i]);
+    for (ll i = 0; i < q; i++)
+    {
+        scanf("%lld", &x);
+        ll k = lo_bound(0, n, x);
+        printf("k: %lld\n", k);
+        ll res = 0;
+        if (k - 1 >= 0)
+        {
+            res = x * (2 * village[k - 1].first - village[n - 1].first) - (2 * village[k - 1].second - village[n - 1].second);
+        }
+        else
+        {
+            res = x * (-1) * (village[n - 1].first) + (village[n - 1].second);
+        }
+        printf("%lld\n", res);
+    }
+
     return 0;
 }
